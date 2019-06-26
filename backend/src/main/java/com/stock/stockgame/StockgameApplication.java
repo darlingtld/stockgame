a268008cf699c2c752de2bd605086f27ddab340e
Lingda <darlingtld@qq.com>
2019-06-26 16:55:05 +0800

    Push backend code

---
+++ backend/src/main/java/com/stock/stockgame/StockgameApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stockgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockgameApplication.class, args);
+    }
+
+}
